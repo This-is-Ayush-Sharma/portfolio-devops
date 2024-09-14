a22063a1c7ef308cb2b453ac87ce783391f6a8cb
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2024-09-14 15:08:03 +0530

    testing

--- devops/cd/deploy.sh
+++ devops/cd/deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+ls
 pid=$(lsof -t -i:${1})
 if [ -n "$pid" ]; then
     echo "Killing process $pid using port ${1}"
     kill -9 $pid
 fi
 pwd
-npx http-server -p 3000 -y &
+npx http-server -p ${1} -y &
 disown
